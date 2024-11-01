b186c2feb660af6a218b3926967ef3267ae22587
narwalayush4 <narwalayush4@gmail.com>
2024-11-01 19:59:16 +0530

    SPM setup

--- SaDeBusinessCardScanner/SaDeBusinessCardScannerApp.swift
+++ SaDeBusinessCardScanner/SaDeBusinessCardScannerApp.swift
@@ -12,24 +12,11 @@ struct SaDeBusinessCardScannerApp: App {
     let persistenceController = PersistenceController.shared
     @Environment(\.scenePhase) var scenePhase
     
-    // for debugging purposes
-    @State private var startTime: Date = Date()
-    
     
     var body: some Scene {
         WindowGroup {
             MainView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
-                .onAppear {
-                    debugPrint("view appeared at: \(Date())")
-                }
-                .onChange(of: scenePhase) {
-                    let endTime = Date()
-                    let elapsedTime = endTime.timeIntervalSince(startTime)
-//                    debugPrint(startTime)
-//                    debugPrint(endTime)
-//                    debugPrint("Time from app launch to main view appearance: \(elapsedTime) seconds")
-                }
         }
         .onChange(of: scenePhase) {
             persistenceController.save()
